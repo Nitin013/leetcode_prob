d5075e3ddad7231febded339d3888cb8a7fe44b0
Nitin013 <100621310+Nitin013@users.noreply.github.com>
2022-05-07 14:43:09 +0530

    Add files via upload

---
+++ my_sort_vector_string.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+void my_sort( vector<string> &s){
+    multimap<int,string> m;
+    multimap<int,string>::iterator itr; 
+    for(int i=0;i<s.size();i++){
+        int sum=0;
+        for(int j=0;j<s[i].size();j++){
+            sum=sum+s[i][j];
+        }
+        m.insert(pair<int,string>(sum,s[i]));
+    }
+    int i=0;
+    itr=m.begin();
+    while(itr!=m.end()){
+        s[i]=itr->second;
+        itr++;
+        i++;
+    }
+}
+int main(){
+   
+    vector<string> s={"nitin","harsh","vikash","tiwari","nitin"};
+    my_sort(s);
+    for(int i=0;i<s.size();i++){
+        cout<<s[i]<<endl;
+    }
+}
