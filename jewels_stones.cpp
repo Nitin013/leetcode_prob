2fb7417beb4b9ca7684b4622bbd7e59f6e45d3c6
Nitin013 <100621310+Nitin013@users.noreply.github.com>
2022-05-07 12:37:31 +0530

    Add files via upload

---
+++ jewels_stones.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+int numJewelsInStones(string jewels, string stones) {
+    vector<int> temp(58,0);
+    for(int i=0;i<stones.size();i++){
+        temp[stones[i]-'A']++;
+    }
+    int ans=0;
+    for(int i=0;i<jewels.size();i++){
+        ans=ans+temp[jewels[i]-'A'];
+    }
+    return ans;
+}
+int main(){
+    string jewels,stones;
+    cout<<"enter the jewels string";
+    cin>>jewels;
+    cout<<"enter the stones string";
+    cin>>stones;
+    cout<<"No.of jewels you have = "<<numJewelsInStones(jewels,stones);
+}
