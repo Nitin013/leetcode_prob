a74e67108dee7726780aaf51542b63c906e6286f
Nitin013 <100621310+Nitin013@users.noreply.github.com>
2022-05-28 02:10:14 +0530

    Update Three_Sum_leetcode.cpp

--- Three_Sum_leetcode.cpp
+++ Three_Sum_leetcode.cpp
@@ -1,15 +1,8 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+# time complexity is o(n^2*logn)
 
 #include <iostream>
 #include<vector>
 #include <bits/stdc++.h>
-#include<algorithm>
 using namespace std;
    vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
