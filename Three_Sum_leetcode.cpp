af7779eccb55f471b6eb57a17a1540636913434d
Nitin013 <100621310+Nitin013@users.noreply.github.com>
2022-05-28 02:49:06 +0530

    Update Three_Sum_leetcode.cpp

--- Three_Sum_leetcode.cpp
+++ Three_Sum_leetcode.cpp
@@ -1,4 +1,4 @@
-# time complexity is o(n^2*logn)
+                                              # time complexity is o(n^2*logn)
 
 #include <iostream>
 #include<vector>
@@ -53,3 +53,73 @@ using namespace std;
             cout<<endl;
         }
     }
+
+##########################################################################################################################################################
+  
+                                                      # Time complexity O(n^2)
+
+#include <iostream>
+#include<vector>
+#include <bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+   vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp(3);
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            int p1=i+1,p2=nums.size()-1;
+            while(p1<p2){
+                if(nums[i]==-nums[p1]-nums[p2]){
+                    temp[0]=nums[i];
+                    temp[1]=nums[p1];
+                    temp[2]=nums[p2];
+                    ans.push_back(temp);
+                    p2--;
+                }
+                else if(nums[i]>-nums[p1]-nums[p2]){
+                    p2--;
+                }
+            else{
+                p1++;
+            }
+            }
+        }
+        for(int i=0;i<ans.size();i++){
+            sort(ans[i].begin(),ans[i].end());
+        }
+        for(int i=0;i<ans.size();i++){
+            for(int j=i+1;j<ans.size();j++){
+                if(ans[i][0]==ans[j][0]&&ans[i][1]==ans[j][1]&&ans[i][2]==ans[j][2]){
+                    ans.erase(ans.begin()+j);
+                    j--;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    int main(){
+        vector<int> temp;
+        cout<<"Enter the element in the array";
+        int n=1;
+        while(n==1){
+            int val;
+            cin>>val;
+            temp.push_back(val);
+            cout<<"1. Enter the element in the array"<<endl<<"2. To get the three sum";
+            cin>>n;
+        }
+        vector<vector<int>> ans;
+        ans=threeSum(temp);
+        cout<<"#####################     Three sum is   ###############"<<endl;
+        for(int i=0;i<ans.size();i++){
+            cout<<i+1<<". ";
+            for(int j=0;j<3;j++){
+                cout<<ans[i][j];
+            }
+            cout<<endl;
+        }
+    }
+
+
