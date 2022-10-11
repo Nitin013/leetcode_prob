e0ed16a1b0fe8988b6ed792bd2d05d70c64cf36d
Nitin013 <100621310+Nitin013@users.noreply.github.com>
2022-10-11 08:36:15 -0700

    Update increasing_triplet_subsequence.cpp

--- increasing_triplet_subsequence.cpp
+++ increasing_triplet_subsequence.cpp
@@ -1,31 +1,67 @@
-// time complexity of this solution is O(n^3)
+
 
 #include<iostream>
 #include<vector>
 #include<algorithm>
 #include<math.h>
 using namespace std;
-class Solution {
+// class Solution {              // time complexity of this solution is O(n^3)
+// public:
+//     bool increasingTriplet(vector<int>& nums) {
+//         for(int i=0;i<nums.size();i++){
+//             int count=0;
+//             int compare=nums[i];
+//             if(nums.size()>=(pow(10,5))){
+//                 return false;
+//             }
+//             for(int j=i+1;j<nums.size();j++){
+//                 compare=nums[i];
+//                 if(compare<nums[j]){
+//                     compare=nums[j];
+//                     for(int k=j;k<nums.size();k++){
+//                           if(compare<nums[k]){
+//                               return true;
+//                           }
+//                     }
+//                 }
+//                 }
+//         }
+//         return false;
+//     }
+// };
+
+class Solution {         // time complexity of this solution is O(n)
 public:
     bool increasingTriplet(vector<int>& nums) {
+        int *smaller=new int[nums.size()];
+        int *greater=new int[nums.size()];
+        int min=0,max=nums.size()-1;
+        smaller[min]=-1,greater[max]=-1;
         for(int i=0;i<nums.size();i++){
-            int count=0;
-            int compare=nums[i];
-            if(nums.size()>=(pow(10,5))){
-                return false;
+            if(nums[i]<=nums[min]){
+                min=i;
+                smaller[i]=-1;
             }
-            for(int j=i+1;j<nums.size();j++){
-                compare=nums[i];
-                if(compare<nums[j]){
-                    compare=nums[j];
-                    for(int k=j;k<nums.size();k++){
-                          if(compare<nums[k]){
-                              return true;
-                          }
-                    }
-                }
-                }
+            else{
+                smaller[i]=min;
+            }
         }
+         for(int i=max-1;i>=0;i--){
+            if(nums[i]>=nums[max]){
+                max=i;
+                greater[i]=-1;
+            }
+            else{
+                greater[i]=max;
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(smaller[i]!=-1&&greater[i]!=-1){
+                return true;
+            }
+        }
+        delete[] smaller;
+        delete[] greater;
         return false;
     }
 };
