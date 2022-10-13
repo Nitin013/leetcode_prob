c62db92d4c5b37768c18ea411a896fbe03b214e1
Nitin013 <100621310+Nitin013@users.noreply.github.com>
2022-10-13 09:49:17 -0700

    Create delete_node_in_a_linklist.cpp

---
+++ delete_node_in_a_linklist.cpp
@@ -0,0 +1,34 @@
+  
+
+#include<iostream>
+  #include<vector>
+  #include<string>
+  using namespace std;
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+      node -> val = node -> next -> val;
+	    node -> next = node -> next -> next;
+    }
+};
+int main(){
+    Solution obj;
+    ListNode* head=new ListNode(4);
+    ListNode* newnode=new ListNode(5);
+    head->next=newnode;
+    ListNode* newnode1=new ListNode(6);
+    head->next->next=newnode1;
+   
+    obj.deleteNode(newnode);
+    while(head!=NULL){
+        cout<<head->val<<endl;
+        head=head->next;
+    }
+    return 0;
+}
