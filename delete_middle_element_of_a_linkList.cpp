d98910fe4ae2d9d297323d2824f9c7e148f04278
Nitin013 <100621310+Nitin013@users.noreply.github.com>
2022-10-14 01:36:05 -0700

    Create delete_middle_element_of_a_linkList.cpp

---
+++ delete_middle_element_of_a_linkList.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        int n=0;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            n++;
+            temp=temp->next;
+        }
+        if(n!=1){
+            n=n/2;
+            temp=head;
+            for(int i=1;i<=n;i++){
+                if(i==n){
+                    temp->next=temp->next->next;
+                }
+                else{
+                    temp=temp->next;
+                }
+            }
+        }
+        else {
+            return NULL;
+        }
+        return head;
+    }
+};
