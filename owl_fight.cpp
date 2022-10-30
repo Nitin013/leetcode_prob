b1f28cae1f7c99e9d864b13ed25dceee7f225dc5
Nitin013 <100621310+Nitin013@users.noreply.github.com>
2022-10-30 12:32:47 -0700

    Add files via upload

---
+++ owl_fight.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int find(int val,vector<int> &vec){
+      while(vec[val]!=val){
+	      val=vec[val];
+      }
+      return val;
+}
+int main() {
+	int num,m;
+	cin >> num>>m;    //Reading input from STDIN
+	vector<int> vec(num+1);
+	for(int j=0;j<=num;j++){
+        vec[j]=j;
+	}
+	for(int i=0;i<m;i++){
+		int val1,val2;
+		cin>>val1>>val2;
+        val1=find(val1,vec);
+		val2=find(val2,vec);
+        if(val1>val2){
+			vec[val2]=val1;
+		}
+		else{
+			vec[val1]=val2;
+		}
+	}
+	int query;
+	cin>>query;
+	while(query){
+		int val1,val2;
+		cin>>val1>>val2;
+		int temp1=val1,temp2=val2;
+		val1=find(val1,vec); 
+		val2=find(val2,vec);
+		if(val1==val2){
+			cout<<"TIE"<<endl;
+		}
+		else if(val1>val2){
+			cout<<temp1<<endl;
+		}
+		else{
+			cout<<temp2<<endl;
+		}
+		query--;
+	}
+}
